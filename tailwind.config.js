652579d8d61e621a4a446027f0f35d8fee324beb
Mrinal Mallik <mrinal@nagorik.tech>
2024-03-12 23:22:13 +0600

    postcss color config issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      extend: {
+        colors: {
+          secondary: {
+            DEFAULT: colors.neutral[200],
+            hover: colors.neutral[300],
+            border: colors.neutral[400],
+            dark: colors.neutral[500],
+            ["dark-hover"]: colors.neutral[900],
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
