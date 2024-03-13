58f28b8eba8dc1cfe47841e154a0cae9039bae3a
Mrinal Mallik <mrinal@nagorik.tech>
2024-03-13 11:21:13 +0600

    chevronleft side

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeft } from "lucide-react";
 import { Button } from "./Button";
 type CategoryPillsProps = {
   categories: string[];
@@ -26,6 +27,19 @@ function CategoryPills({
             {category}
           </Button>
         ))}
+      </div>
+      <div
+        className="absolute left-0 top-1/2 -translate-y-1/2 
+        bg-gradient-to-r from-white  from-50%
+        to-transparent w-24 h-full"
+      >
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-full aspect-square w-auto p-1.5"
+        >
+          <ChevronLeft />
+        </Button>
       </div>
     </div>
   );
