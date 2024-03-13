782e48e361f329f3f4d0ad7e607fed9d70040fe8
Mrinal Mallik <mrinal@nagorik.tech>
2024-03-13 07:39:29 +0600

    nav menu

--- src/layouts/PageHeader.tsx
+++ src/layouts/PageHeader.tsx
@@ -1,5 +1,5 @@
 import logo from "../assets/logo.png";
-import { Menu, Upload, Bell, User } from "lucide-react";
+import { Menu, Upload, Bell, User, Mic, Search } from "lucide-react";
 import { Button } from "../components/Button";
 
 function PageHeader() {
@@ -18,8 +18,23 @@ function PageHeader() {
           <img src={logo} className="h-6" />
         </a>
       </div>
-      <div></div>
-      <div>
+      <form className="flex gap-4 flex-grow justify-center">
+        <div className="flex flex-grow max-w-[600px]">
+          <input
+            type="search"
+            placeholder="Search"
+            className="rounded-l-full border border-secondary-border 
+            shadow-inner shadow-secondary py-1 px-4 text-lg w-full"
+          />
+          <Button>
+            <Search />
+          </Button>
+        </div>
+        <Button type="button" size="icon" className="flex-shrink-0">
+          <Mic />
+        </Button>
+      </form>
+      <div className="flex flex-shrink-0 md:gap-2">
         <Button size="icon" variant="ghost">
           <Upload />
         </Button>
